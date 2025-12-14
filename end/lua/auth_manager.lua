a8fd25233b1e75c74d83584caba958e0c0a3cdeb
ztf <ztfcharlie@163.com>
2025-12-14 10:48:48 +0800

    t

--- end/lua/auth_manager.lua
+++ end/lua/auth_manager.lua
@@ -24,8 +24,7 @@ local OPENAI_WHITELIST = {
     "^/v1/audio/speech",
     "^/v1/audio/transcriptions",
     "^/v1/audio/translations",
-    "^/v1/video/",
-    "^/v1/videos/",
+    "^/v1/videos",
     "^/v1/models"
 }
 
